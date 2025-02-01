be501eb5b05b7b3a80365c30ea44c15b5c83b0bb
samss084 <131480264+samss084@users.noreply.github.com>
2025-02-01 08:35:32 -0700

    Update customerfile.cs

--- customerfile/customerfile.cs
+++ customerfile/customerfile.cs
@@ -7,20 +7,20 @@ namespace Data.Repository
 {
     public class CustomerAccountRepository : Repository<CustomerAccount>
     {
-        public void AddCustomerAccount(CustomerAccount customerAccount)
+        public  AddCustomerAccount(CustomerAccount customerAccount)
         {
 
-            customerAccount.IsClosed = false;
+            
             Save(customerAccount);
         }
 
         public IList<CustomerAccount> GetAccountsByFirstName(string accountName)
         {
             List<CustomerAccount> account = new List<CustomerAccount>();
-            ISession session = NHibernateHelper.Session;
+            ISession session = Helper.Session;
             ICriteria criteria = session.CreateCriteria(typeof(CustomerAccount));
-            criteria.Add(Restrictions.Eq("AccountName", accountName));
-            criteria.AddOrder(Order.Desc("ID"));
+            criteria.Add("AccountName", accountName));
+            criteria.Add request"ID"));
             account = criteria.List<CustomerAccount>() as List<CustomerAccount>;
 
             string sqlQuery = string.Format("select * from CustomerAccount where AccountName = {0} order by ID desc", accountName);
